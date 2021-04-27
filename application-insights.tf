81b0716ba5bc49c83613f882627851843ee3cb5c
jackmaloney <jack.maloney@transformuk.com>
2021-04-27 16:35:52 +0100

    Set sensitive to true

--- application-insights.tf
+++ application-insights.tf
@@ -13,9 +13,9 @@ resource "azurerm_key_vault_secret" "app_insights_key" {
   name         = "AppInsightsInstrumentationKey"
   value        = azurerm_application_insights.appinsights.instrumentation_key
   key_vault_id = module.sscs-vault.key_vault_id
-  sensitive = true
 }
 
 output "appInsightsInstrumentationKey" {
   value = azurerm_application_insights.appinsights.instrumentation_key
+  sensitive = true
 }
